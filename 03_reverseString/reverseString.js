a6ed5fbf32a2ff5e59c01888e1ccd0d09a14b5f9
Reece Vela <reecevela@outlook.com>
2022-04-15 11:16:43 -0500

    Finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let array = [];
+  let i = 0;
+  let finalString = '';
+  while (array.length < string.length) {
+    array[i] = string.substr(i, 1);
+    i++;
+  }
+  array.reverse();
+  array.forEach(letter => {
+    finalString += letter;
+  });
+  return finalString;
 };
 
 // Do not edit below this line
