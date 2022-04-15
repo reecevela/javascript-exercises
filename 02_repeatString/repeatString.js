f3dc5f74edc130e360d6ee3767b5563b640a39a8
Reece Vela <reecevela@outlook.com>
2022-04-15 10:57:08 -0500

    Complete 02_repeatSring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str = '', loops = 0) {
+  let finalString = '';
 
+  str.toString();
+
+  while (loops > 0) {
+    finalString += str;
+    loops--;
+  }
+
+  return (loops < 0 ? 'ERROR' : finalString);
 };
 
 // Do not edit below this line
