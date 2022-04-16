a595c8a7a0261526dcc0b5578fd0d96eb188d00c
Reece Vela <reecevela@outlook.com>
2022-04-15 19:29:09 -0500

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+  if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+  let higher;
+  let lower;
+
+  switch (a > b) {
+    case true:
+      higher = a;
+      lower = b;
+      break;
+    default:
+      higher = b;
+      lower = a;
+  }
+
+  while (lower <= higher) {
+    sum += lower++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
