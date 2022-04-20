2e4b1291e991bb12ed92957f69be0c2543b342f0
Reece Vela <reecevela@outlook.com>
2022-04-20 18:38:27 -0500

    Create all functions for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+	numbers.forEach(number => {
+    sum += number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  numbers.forEach(number => {
+    total *= number;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number > 0){
+    let total = 1;
+    while (number > 0) {
+      total *= number--;
+    }
+    return total;
+  } 
+  return 1;
 };
 
 // Do not edit below this line
