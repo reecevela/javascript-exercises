f09c65b9e1f50950e7804f864db87f7d83a6279f
Reece Vela <reecevela@outlook.com>
2022-04-20 19:13:26 -0500

    Complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  string = string.toLowerCase().replace(/[^a-z0-9]+/g, '');
+
+  string.replace("!", "");
+  string.replace(" ", "");
+  let reverse = reverseString(string);
+
+  return reverse == string ? true : false;
+};
+
+const reverseString = function(string) {
+  let array = [];
+  let i = 0;
+  let finalString = '';
+  while (array.length < string.length) {
+    array[i] = string.substr(i, 1);
+    i++;
+  }
+  array.reverse();
+  array.forEach(letter => {
+    finalString += letter;
+  });
+  return finalString;
 };
 
 // Do not edit below this line
