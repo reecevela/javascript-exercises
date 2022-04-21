538aa3e7b62382d26dffa0cda8f33606e77b7899
Reece Vela <reecevela@outlook.com>
2022-04-20 21:20:51 -0500

    Create Fibonnaci sequence function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  let engine;
+  let trainCar = 1;
+  let caboose = 0;
 
+  while (index > 0) {
+    engine = trainCar + caboose;
+    console.log("engine is " + engine);
+    //console.log("trainCar is " + trainCar);
+    //console.log("caboose is " + caboose);
+    caboose = trainCar;
+    trainCar = engine;
+    if (--index <= 1) {
+      return engine;
+    }
+  }
+  return "OOPS";
 };
 
 // Do not edit below this line
