e97578e3b0c4dd4c25654ca308808eaeeea6c34a
Reece Vela <reecevela@outlook.com>
2022-04-21 19:17:32 -0500

    Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestPerson = {
+    age: 0,
+    name: "al"
+  };
 
+  people.forEach(person => {
+    if (calcAge(person.yearOfBirth, person.yearOfDeath) > oldestPerson.age) {
+      oldestPerson.name = person.name;
+      oldestPerson.age = calcAge(person.yearOfBirth, person.yearOfDeath);
+    }
+  });
+
+  return oldestPerson;
+};
+const calcAge = function (birthYear, deathYear) {
+  const today = new Date ();
+  const thisYear = today.getFullYear();
+
+  
+  deathYear = deathYear ?? thisYear;
+  const age = deathYear - birthYear;
+  return age;
 };
 
 // Do not edit below this line
